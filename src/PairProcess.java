6595dab8a38d41078bd56110c7b9a149bad291d7
josiarod <josiarodriguez@gmail.com>
2020-01-22 16:33:53 -0500

    Math operations

---
+++ src/PairProcess.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PairProcess {
+
+    public static void main(String[] args) {
+        int num1, num2, sum, product, average;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("First number? : ");
+        num1 = keyboard.nextInt();
+
+        System.out.print("Second number? : ");
+        num2 = keyboard.nextInt();
+
+        sum = num1 + num2;
+        product = num1 * num2;
+        average = sum/2;
+
+        if(sum > 200)
+            System.out.println("The sum is: " +sum+"*");
+        else{
+            System.out.println("The sum is: " + sum);
+        }
+
+        System.out.println("The product is: " +product);
+        System.out.println("The average is:" +average);
+
+    }
+
+}
